cde05d28e4494832b8e9e1b6df76ab23cb785700
Tiiperi <38816054+Tiiperi@users.noreply.github.com>
2018-04-28 23:54:05 +0300

    Level loader

--- Water/Assets/Scripts/NewScene.cs
+++ Water/Assets/Scripts/NewScene.cs
@@ -13,16 +13,4 @@ public class NewScene : MonoBehaviour {
         }
     }
 }
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class LevelComplete : MonoBehaviour
-{
-
-    public void LoadNextLevel()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-}*/
