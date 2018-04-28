d6557511686657031381b825d0a73de8e4d56db3
astroxin <astrotus@gmail.com>
2018-04-28 19:55:58 +0300

    Täs olis koodi jumaluutta
    Jep

---
+++ Blobby/Assets/propellerRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class propellerRotate : MonoBehaviour {
+
+    public float propellerRate;
+    public Vector3 propellerDirection;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate((propellerDirection * propellerRate) * Time.deltaTime);
+
+	}
+}
