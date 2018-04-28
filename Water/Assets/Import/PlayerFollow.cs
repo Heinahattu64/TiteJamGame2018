dce8b4dd68336281292e833f82656a99ea4a7509
Tiiperi <38816054+Tiiperi@users.noreply.github.com>
2018-04-28 20:12:49 +0300

    PreUpdate

---
+++ Water/Assets/Import/PlayerFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position; 
+	}
+
+    void Update()
+    {
+        MoveCamera();
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+    }
+
+    void MoveCamera()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Holding");
+        }
+    }
+}
---
+++ Water/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{ 
+
+    public float rotaterate;
+    private Rigidbody rb;
+    
+    public float jumpPower;
+    
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+    
+        MoveBlobby();
+    }
+
+    void MoveBlobby()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotaterate;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * rotaterate;
+
+        Vector3 movement = new Vector3(x, 0.0f, z);
+
+        rb.AddForce(movement);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector3 jump = new Vector3(0.0f, jumpPower, 0f);
+
+            rb.AddForce(jump);
+        }
+    }
+}
