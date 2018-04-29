5d05d4c06aab2f19231ac2698a5a26182c61ea5b
astroxin <astrotus@gmail.com>
2018-04-29 12:08:01 +0300

    Novement

--- Blobby/Assets/CamAnchor.cs
+++ Blobby/Assets/CamAnchor.cs
@@ -6,15 +6,34 @@ public class CamAnchor : MonoBehaviour {
 
     public Transform player;
     public Transform camMain;
+    public float rotaterate;
+    public float x;
+    public float z;
+
+    private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent <Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Vector3 test = camMain.position;
+        Quaternion camRotation = camMain.localRotation;
+        //anchor.rotation = Quaternion.LookRotation(test);
+
+        transform.localRotation = Quaternion.Slerp(camRotation, transform.rotation, Time.deltaTime);
+    }
+
+    public void MovementTry()
+    {
+        x = Input.GetAxis("Horizontal") * Time.deltaTime * rotaterate;
+        z = Input.GetAxis("Vertical") * Time.deltaTime * rotaterate;
+    }
+
+    public void LateUpdate()
+    {
         transform.position = player.transform.position;
-        transform.rotation = camMain.transform.rotation;
-	}
+    }
 }
